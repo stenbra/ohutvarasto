c482dcff219829c428af2718eb116d5c0b272f62
Rastenbe <stenbra7@gmail.com>
2022-11-07 15:53:05 +0200

    branch covarage is now at 100% for varasto.py

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,47 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+    
+    def test_lisays_maara_negatiivi(self):
+        self.varasto.lisaa_varastoon(8)
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 8)
+    
+    def test_lisays_liika(self):
+        self.varasto.lisaa_varastoon(100)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+    
+    def test_ottaminen_liika_palauttaa_kaikki_mita_voidan(self):
+        self.varasto.lisaa_varastoon(8)
+        
+        saatu_maara = self.varasto.ota_varastosta(9)
+        
+        self.assertAlmostEqual(saatu_maara,8)
+        
+    def test_ottaminen_maara_on_negatiivi(self):
+        self.varasto.lisaa_varastoon(8)
+        
+        saatu_maara = self.varasto.ota_varastosta(-1)
+        
+        self.assertAlmostEqual(saatu_maara,0)
+        
+    def test_alku_saldo_negatiivi(self):
+        varasto = Varasto(10,-1)
+        
+        self.assertAlmostEqual(varasto.saldo,0)
+        
+    def test_alku_saldo_liika(self):
+        varasto = Varasto(10,200)
+        
+        self.assertAlmostEqual(varasto.saldo,10)
+        
+    def test_tilavuus_negatiivi(self):
+        varasto = Varasto(-10)
+        self.assertAlmostEqual(varasto.tilavuus,0)
+        
+    def test_tilavuus_nolla(self):
+        varasto = Varasto(0)
+        self.assertAlmostEqual(varasto.tilavuus,0)
+        
+    def test__str__(self):
+        self.assertAlmostEqual(self.varasto.__str__(),f"saldo = {self.varasto.saldo}, vielä tilaa {self.varasto.paljonko_mahtuu()}")
